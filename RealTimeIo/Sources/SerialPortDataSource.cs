28c9a6db8cc9ef947800b82d6a5d02807fedffdd
Ilya Belan <iliyars@mail.ru>
2025-11-15 22:25:11 +0300

    Add SerialPortDataSource

--- RealTimeIo/Sources/SerialPortDataSource.cs
+++ RealTimeIo/Sources/SerialPortDataSource.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Channels;
 using System.Threading.Tasks;
 using System.IO.Ports;
+using System.Runtime.InteropServices;
 
 namespace RealTimeIo.Sources
 {
@@ -18,21 +19,89 @@ namespace RealTimeIo.Sources
         private Task? _readTask;
 
 
-        public ChannelReader<byte[]> Bloks => throw new NotImplementedException();
+        public ChannelReader<byte[]> Bloks => _channel.Reader;
 
-        public ValueTask DisposeAsync()
+
+        public SerialPortDataSource(SerialPort port, int bufferSize = 256, int channelCapacity = 64)
         {
-            throw new NotImplementedException();
+            _port = port;
+            _bufferSize = bufferSize;
+
+            _channel = Channel.CreateBounded<byte[]>(new BoundedChannelOptions(channelCapacity)
+            {
+                SingleWriter = true,
+                SingleReader = false,
+                FullMode = BoundedChannelFullMode.Wait
+            });
+        }
+
+
+
+
+
+        public async ValueTask DisposeAsync()
+        {
+            _cts.Cancel();
+            if (_readTask != null)
+                await _readTask;
+
+            if (_port.IsOpen)
+                _port.Close();
+
+            _cts.Dispose();
         }
 
         public void Start()
         {
-            throw new NotImplementedException();
+            if (_readTask != null)
+                throw new InvalidOperationException("Already started");
+
+            if(!_port.IsOpen)
+                _port.Open();
+
+            _readTask = Task.Run(ReadLoopAsync);
+        }
+
+        private async Task ReadLoopAsync()
+        {
+            var token = _cts.Token;
+            var stream = _port.BaseStream;
+
+            try
+            {
+                while(!token.IsCancellationRequested)
+                {
+                    var buffer = new byte[_bufferSize];
+                    int read = await stream.ReadAsync(buffer.AsMemory(0, _bufferSize), token);
+
+                    if (read <= 0)
+                        continue;
+
+                    if(read < _bufferSize)
+                    {
+                        var trimed = new byte[read];
+                        Array.Copy(buffer, trimed, read);
+                        buffer = trimed;
+                    }
+
+                    if (!await _channel.Writer.WaitToWriteAsync(token))
+                        break;
+
+                    await _channel.Writer.WriteAsync(buffer, token);
+                }
+            }catch(OperationCanceledException) 
+            {
+
+            }
+            finally
+            {
+                _channel.Writer.TryComplete();
+            }
         }
 
         public void Stop()
         {
-            throw new NotImplementedException();
+            _cts.Cancel();
         }
     }
 }
